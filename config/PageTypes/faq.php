eb7bc5785e5cc02b80c25cc6c60fccdb52eb69ab
tatoGI <workspace.tato@gmail.com>
2025-04-24 14:08:55 +0400

    Add FAQ page configuration with submenu items

---
+++ config/PageTypes/faq.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'id' => 8,
+    'type' => 8,
+    'name' => 'Faq Page',
+    'items' => [
+        ['label' => 'Submenu 1', 'url' => '/submenu1'],
+        ['label' => 'Submenu 2', 'url' => '/submenu2'],
+    ],
+];
