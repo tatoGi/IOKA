6afdd3893794989f1933d2dcbfc618435d472096
tatoGI <workspace.tato@gmail.com>
2025-04-24 17:10:54 +0400

    change

--- routes/api.php
+++ routes/api.php
@@ -35,5 +35,6 @@ use Illuminate\Support\Facades\Route;
     Route::get('/rental/related', [FrontendController::class, 'getRelatedRental']);
     Route::get('/policies', [FrontendController::class, 'policy']);
     Route::get('/faqs', [FrontendController::class, 'getFAQ']);
+    // for github //
     Route::get('/searchOffplanLocations', [FrontendController::class, 'searchOffplanLocations']);
 
